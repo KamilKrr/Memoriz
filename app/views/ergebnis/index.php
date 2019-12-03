3015da198567b47ea4810974bb4510c0eae49607
Tim Schreiber <tim.schreiber@outlook.com>
2019-12-03 17:17:06 +0100

    new Ergebnis component

---
+++ app/views/ergebnis/index.php
@@ -0,0 +1,17 @@
+<link href="/public/css/ergebnis.css" rel="stylesheet">
+
+<div class="columns">
+    <div class="column is-5 columns card-container">
+        <div>
+            <div class="memory-card">
+
+            </div>
+            <div class="memory-card top-card">
+
+            </div>
+        </div>
+    </div>
+    <div class="column info-text is-full-mobile">
+        <p>Ipsum irure laboris exercitation anim pariatur sunt culpa incididunt ullamco amet nostrud consectetur. Officia nostrud ipsum incididunt do sunt esse. Adipisicing occaecat pariatur aute voluptate anim ipsum mollit sint sunt fugiat commodo. Magna exercitation aliquip consectetur ad elit tempor id aliquip quis commodo pariatur commodo. Elit eiusmod laboris ullamco reprehenderit veniam nostrud non tempor excepteur. Sunt enim dolor dolore occaecat.</p>
+    </div>
+</div>
